44777f6a9c1562b661c05bb3532337ef48886f64
13Katrin <marsik.cat@mail.ru>
2022-08-16 14:51:25 +0300

    izmemeniemili

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1200;
+        int bonus = ticket / 20;
+        System.out.println("Вам начислено: " + bonus + " бонусных миль");
+    }
+}
